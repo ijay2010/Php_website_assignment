e2c73c8ae3038e8f535231779e278b5c3d2641c7
ijay2010 <peaceumeh5@gmail.com>
2023-06-15 11:07:17 -0700

    last commit

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 //add smooth scrolling to navigation links
 const navLinks = document.querySelectorAll('nav a');
 navLinks.forEach(link => {
-    link.addEventListener('click, e =>')
+    link.addEventListener('click', e =>{
     e.preventDefault();
     const targetsection = document.querySelector(link.getAttribute('href'));
     targetsection.scrollIntoView({
         behavior: "smooth"
     });
 });
+});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
